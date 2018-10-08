5a5239b9687c45ba4601bfc13bca6c5ff2f312d0
James Li <gli38@ucsc.edu>
2018-10-07 18:11:58 -0700

    gettysburg.txt

--- HW1/test/NQueens.java
+++ HW1/test/NQueens.java
@@ -1,19 +1,8 @@
-import java.io.*;
 import java.util.Scanner;
-
-class Coordinate{
-   public int x;    // x-coordinate
-   public int y;    // y-coordinate
-
-   public Coordinate(int x, int y){   // constructor
-      this.x = x;
-      this.y = y;
-   }
-}
 
 class NQueens{
    static int size;     // the size of the chess board
-   static Coordinate[] chessArray;
+   static int[] chessArray;
    static int xcoord;
    static int ycoord;
    static boolean noSol = true;   // check if has solution
@@ -24,9 +13,9 @@ class NQueens{
       xcoord = sc.nextInt() - 1;      // get the x-coord
       ycoord = sc.nextInt() - 1;      // get the y-coord
 
-      chessArray = new Coordinate[size];
+      chessArray = new int[size];
       for (int i=0; i<size; i++)    // initialize queens
-         chessArray[i] = new Coordinate(i, i);
+         chessArray[i] = i;
 
       doPermutation(size);
       if (noSol) System.out.println("No solution");
@@ -44,33 +33,32 @@ class NQueens{
    static void rotate(int newSize){
       int j;
       int position = size - newSize;       // start from the position
-      int temp = chessArray[position].y;       // take the first element out
+      int temp = chessArray[position];       // take the first element out
       for(j=position+1; j<size; j++)         // shift all elements from
-         chessArray[j-1].y = chessArray[j].y;          // position+1 to left by 1
-      chessArray[j-1].y = temp;           // put the first element into the 
-                                     // last slot
+         chessArray[j-1] = chessArray[j];          // position+1 to left by 1
+      chessArray[j-1] = temp;           // put the first element into the last slot
    }
 
-   static boolean isSolution(Coordinate[] A){    // test for diagonal attacks
+   static boolean isSolution(int[] A){    // test for diagonal attacks
       for(int i=0; i<size-1; i++){
          for(int j=i+1; j<size; j++){
-            if (A[i].x-A[j].x == A[i].y-A[j].y
-               || A[i].x-A[j].x == A[j].y-A[i].y) return false;
+            if (i-j == A[i]-A[j]
+               || i-j == A[j]-A[i]) return false;
          }
       }
       return true;
    }
 
    static boolean finalAnswer(int x, int y){    // make the solution match the input
-      return chessArray[x].y == y; 
+      return chessArray[x] == y; 
    }
 
    static void printAnswer(){      // print out the final answer
       if(isSolution(chessArray) && finalAnswer(xcoord, ycoord)){
          for(int j=0; j<size; j++){
             noSol = false;
-            System.out.print(chessArray[j].x+1 + " ");
-            System.out.print(chessArray[j].y+1 + " ");
+            System.out.print(j+1 + " ");
+            System.out.print(chessArray[j]+1 + " ");
          }
       System.out.println();
       }
