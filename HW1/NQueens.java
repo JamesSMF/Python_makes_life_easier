c2e78c9dbd5c3b481fe8bac02ee525096d3413ec
James Li <gli38@ucsc.edu>
2018-10-08 09:11:04 -0700

    HW1

--- HW1/NQueens.java
+++ HW1/NQueens.java
@@ -1,31 +1,67 @@
 import java.util.Scanner;
+import java.io.*;
 
 class NQueens{
-   static int size;     // the size of the chess board
+   static int[] theQueue;     // the queue is for the input
+   static int front;    // the front index of the queue
+   static int rear;     // the rear index of the queue
+   static int nElems;   // the number of elems in the queue
+
    static int[] chessArray;
+   static int size;     // the size of the chess board
    static int xcoord;
    static int ycoord;
-   static boolean noSol = true;   // check if has solution
+   static boolean noSol;   // check if has solution
+   static int[] result = new int[180];    // the result array
+   static int nextEmpty = 0;    // the index of next empty slot in result
+   static int nextElem = 0;     // the index of the next used slot
 
-   public static void main(String[] args){
-      Scanner sc = new Scanner(System.in);
-      size = sc.nextInt();        // get the size
-      xcoord = sc.nextInt() - 1;      // get the x-coord
-      ycoord = sc.nextInt() - 1;      // get the y-coord
+   public static void main(String[] args) throws IOException{
+      theQueue = new int[120];
+      nElems = 0;
+      front = 0;
+      rear = -1;
+      int[] Line = new int[3];     // store the 3 data on each line
 
-      chessArray = new int[size];
-      for (int i=0; i<size; i++)    // initialize queens
-         chessArray[i] = i;
+      Scanner sc = new Scanner(new File(args[0]));
+      PrintWriter out = new PrintWriter(new FileWriter(args[1]));
+      while (sc.hasNextInt())           // while has next int
+         push(sc.nextInt());           // push into the queue
 
-      doPermutation(size);
-      if (noSol) System.out.println("No solution");
+      while (!isEmpty()){
+         for(int k=0; k<3; k++)
+            Line[k] = pop();
+         size = Line[0];
+         xcoord = Line[1] - 1;
+         ycoord = Line[2] - 1;
+
+         chessArray = new int[size];
+         for (int i=0; i<size; i++)    // initialize queens
+            chessArray[i] = i;
+         
+         noSol = true;         // initialize noSol
+
+         doPermutation(size);
+         if (noSol){
+            out.println("No solution");
+            out.flush();
+            continue;
+         }
+
+         for (int i=nextElem; i<nextElem+2*size; i++)
+            out.print(result[i] + " ");
+         out.flush();
+         out.println();
+         nextElem += 2*size;
+      }
    }
 
    static void doPermutation(int newSize){
       if(newSize == 1) return;    // size is too small
       for(int j=0; j<newSize; j++){
          doPermutation(newSize-1);
-         if(newSize == 2) printAnswer();
+         if(newSize == 2) storeAnswer();
+         if(noSol == false) return;     // only print one possible sol out
          rotate(newSize);
       }
    }
@@ -53,16 +89,30 @@ class NQueens{
       return chessArray[x] == y; 
    }
 
-   static void printAnswer(){      // print out the final answer
+   static void storeAnswer(){      // print out the final answer
       if(isSolution(chessArray) && finalAnswer(xcoord, ycoord)){
-         for(int j=0; j<size; j++){
+         for(int j=0; j<2*size; j += 2){
             noSol = false;
-            System.out.print(j+1 + " ");
-            System.out.print(chessArray[j]+1 + " ");
+            result[nextEmpty++] = (j+2)/2;
+            result[nextEmpty++] = chessArray[j/2]+1;
          }
-      System.out.println();
       }
    }
+
+   static void push(int j){      // push one int into the queue
+      if(rear == 119) rear = -1;
+      theQueue[++rear] = j;
+      nElems++;
+   }
+
+   static int pop(){     // truncanate an elem out
+      int temp = theQueue[front++];
+      if(front==120) front = 0;
+      nElems--;
+      return temp;
+   }
+
+   static boolean isEmpty() { return nElems==0; }
 }
 
 
