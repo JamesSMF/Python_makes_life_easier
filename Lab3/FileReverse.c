25d1600f82c36115c0b4ed20d246e4aaf13808ab
James Li <gli38@ucsc.edu>
2018-10-10 20:06:33 -0700

    HW1

--- Lab3/FileReverse.c
+++ Lab3/FileReverse.c
@@ -1,14 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
-char theStack[30];
-int top = -1;
+char *stringReverse(char *str){
+    char c, *front, *back;
+
+   /* Make sure *str != '\0' and str is not null. */
+   if(!str || !*str)
+      return str;
+
+   /* Swap the front and the back   (namely reverse) */
+   for(front=str,back=str+strlen(str)-1;front < back;front++,back--){
+      c=*front;
+      *front=*back;
+      *back=c;
+   }
+    return str;
+}
 
 int main(int argc, char* argv[]){
-	FILE* in;
-	FILE* out;
+   FILE* in;  /* file handle for input */  
+   FILE* out; /* file handle for output */
+   char word[256]; /* char array to store words from input file */
 
-   if( argc != 3 ){         /* check the number of input files */
+   /* check command line for correct number of arguments */
+   if( argc != 3 ){
       printf("Usage: %s <input file> <output file>\n", argv[0]);
       exit(EXIT_FAILURE);
    }
@@ -22,25 +38,23 @@ int main(int argc, char* argv[]){
 
    /* open output file for writing */
    out = fopen(argv[2], "w");
-   if( out==NULL )
+   if( out==NULL ){
       printf("Unable to write to file %s\n", argv[2]);
-
-   /* read words from input file, print on separate lines to output file */
-   while( fscanf(in, " %s", theStack) != EOF ){
-      fprintf(out, "%s\n", theStack);
+      exit(EXIT_FAILURE);
    }
 
+   /* read words from input file, print on separate lines to output file*/
+   while( fscanf(in, " %s", word) != EOF ){
+      stringReverse(word);
+      fprintf(out, "%s\n", word);
+   }
+
+   /* close input and output files */
    fclose(in);
    fclose(out);
 
    return(EXIT_SUCCESS);
 }
-
-void push(char ch){
-	top = top + 1;
-	theStack[top] = ch;
-}
-
 
 
 
