3e3bb58eed25e795199af97e8673f67b2080a5e5
James Li <gli38@ucsc.edu>
2018-10-28 15:48:14 -0700

    README

---
+++ Lab4/test1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]){
+   FILE* in;  /* file handle for input */  
+   FILE* out; /* file handle for output */
+   char* A;  /* This is a pointer pointing at the beginning of the array */
+
+/*   while(fscanf(in, "%c", A) != EOF){
+      A = (char*)malloc(256 * sizeof(char));
+      if (A == NULL){
+         fprintf(stderr, "malloc failed \n");
+         exit(EXIT_FAILURE);
+      }
+
+       To this point, the program requested some memories
+
+      free(A);
+   }   */
+}
+
