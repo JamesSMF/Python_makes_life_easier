f7d2adecb60631d92eeed40560b5e072a74706b5
James Li <gli38@ucsc.edu>
2018-12-06 20:24:45 -0800

    Add an example of printing on the same line in python

---
+++ Examples/python/printReverse.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+
+class Stack(object):
+   theList = [1]
+   del theList[0]
+   elem = 0
+
+   def push(self, num):
+      self.theList.append(num)
+      self.elem = self.elem + 1
+
+   def pop(self):
+      self.elem = self.elem - 1
+      temp = self.theList[self.elem]
+      del self.theList[self.elem]
+      return temp
+
+   def isEmpty(self):
+      if self.elem == 0:
+         return True
+      else:
+         return False
+
+   def printStack(self):
+      for i in self.theList:
+         print(i, end = '')
+
+
+theString = "How's your day going?"
+theStack = Stack()
+for char in  theString:
+   theStack.push(char)
+
+theStack.printStack();
+print('', end = '\n');
