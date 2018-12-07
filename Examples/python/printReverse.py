dd4c692827787a81aed084153e9ea3da561bd46b
James Li <gli38@ucsc.edu>
2018-12-06 20:46:10 -0800

    Upload an example of OOP in python

--- Examples/python/printReverse.py
+++ Examples/python/printReverse.py
@@ -2,26 +2,26 @@ from __future__ import print_function
 
 class Stack(object):
    theList = [1]
-   del theList[0]
-   elem = 0
+   del theList[0]   # Now it is an empty list
+   elem = 0         # has 0 element at first
 
-   def push(self, num):
+   def push(self, num):             # push one item into the stack
       self.theList.append(num)
       self.elem = self.elem + 1
 
-   def pop(self):
+   def pop(self):                  # pop the top item out
       self.elem = self.elem - 1
       temp = self.theList[self.elem]
       del self.theList[self.elem]
       return temp
 
-   def isEmpty(self):
+   def isEmpty(self):             # check if the stack is empty
       if self.elem == 0:
          return True
       else:
          return False
 
-   def printStack(self):
+   def printStack(self):         # print the stack from bottom to top
       for i in self.theList:
          print(i, end = '')
 
