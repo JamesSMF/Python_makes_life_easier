35bb7b34135ec29a0923b3d8ac412a1ef6fa7d9f
JamesSMF <39791113+JamesSMF@users.noreply.github.com>
2018-12-11 00:05:43 -0800

    Update monkeySort.py

--- python/monkeySort.py
+++ python/monkeySort.py
@@ -5,14 +5,14 @@ def is_sorted(theList):
 	for i in range(1, len(theList)):       # traverse the entire list
 		if theList[i] < theList[i-1]:  # if some item is smaller than the previous one
 			return False           # it is not sorted
-	return True                   # if all items are in their own correct place, return true
+	return True      # if all items are in their own correct place, return true
 # end def
 
 s = raw_input("Please enter the numbers, splited by space: \n")    # get the input as string
-data = map(int, s.split())                       # convert the string into a list of numbers
+data = map(int, s.split())       # convert the string into a list of numbers
 
-while not is_sorted(data):                       # while the list is not sorted
-	random.shuffle(data)                     # shuffle all the items in the list
+while not is_sorted(data):       # while the list is not sorted
+	random.shuffle(data)     # shuffle all the items in the list
 # end while
 
-print data                                       # print the sorted list
+print data      # print the sorted list
