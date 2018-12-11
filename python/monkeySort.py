c6a6ec4f68ec41432cc92940a03f2f735843f795
JamesSMF <39791113+JamesSMF@users.noreply.github.com>
2018-12-11 00:04:54 -0800

    Update monkeySort.py

--- python/monkeySort.py
+++ python/monkeySort.py
@@ -1,17 +1,18 @@
 import random
 
+# This function checks if the list is sorted by increasing order
 def is_sorted(theList):
-	for i in range(1, len(theList)):
-		if theList[i] < theList[i-1]:
-			return False
-	return True
+	for i in range(1, len(theList)):       # traverse the entire list
+		if theList[i] < theList[i-1]:  # if some item is smaller than the previous one
+			return False           # it is not sorted
+	return True                   # if all items are in their own correct place, return true
 # end def
 
-s = raw_input("Please enter the numbers, splited by space: \n")
-data = map(int, s.split())
+s = raw_input("Please enter the numbers, splited by space: \n")    # get the input as string
+data = map(int, s.split())                       # convert the string into a list of numbers
 
-while not is_sorted(data):
-	random.shuffle(data)
+while not is_sorted(data):                       # while the list is not sorted
+	random.shuffle(data)                     # shuffle all the items in the list
 # end while
 
-print data
+print data                                       # print the sorted list
