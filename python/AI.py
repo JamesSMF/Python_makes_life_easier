758fca852434440ef1d3b5895fc86e4cc0ff9582
JamesSMF <39791113+JamesSMF@users.noreply.github.com>
2019-02-10 12:51:06 -0800

    Create AI.py

---
+++ python/AI.py
@@ -0,0 +1,21 @@
+import re
+
+while(True):
+	userInput = raw_input()
+		if userInput=='q':
+			break
+
+		else:
+			userInput = re.sub("\?", "!", userInput)
+			userInput = re.sub("What's your name", "I am Sirai", userInput)
+			userInput = re.sub("What is your name", "I am Sirai", userInput)
+			userInput = re.sub("Who are you", "I am Sirai", userInput)
+			userInput = re.sub("What can you do", "I can do anything", userInput)
+			userInput = re.sub("Can you", "I can", userInput)
+			userInput = re.sub("You", "I", userInput)
+			userInput = re.sub("Are you", "Yes, I am", userInput)
+			userInput = re.sub("you", "I", userInput)
+			userInput = re.sub(" me", " you", userInput)
+
+			#  userInput = re.sub("", "", userInput)
+			print userInput+'\n'
