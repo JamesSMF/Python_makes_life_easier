f5aebc06d44557c8e51a1f2be142c5300b2e0c68
Super James <liguangyao@eduroam-169-233-215-190.ucsc.edu>
2019-04-03 11:47:46 -0700

    update sql.py

--- python/sql.py
+++ python/sql.py
@@ -2,33 +2,36 @@
 
 # importing the module
 import sqlite3
+import os
 
 # connect withe the myTable database
-connection = sqlite3.connect("myTable.db")
+connection = sqlite3.connect("vocabulary.db")
 
+# This statement removes 'u' at the beginning of the output
+connection.text_factory = str
 crsr = connection.cursor()
 
+# if the database does not exist, create a new one.
 # SQL command to create a table in the database
-sql_command = """CREATE TABLE emp (
-staff_number INTEGER PRIMARY KEY,
-fname VARCHAR(20),
-lname VARCHAR(30),
-gender CHAR(1),
-joining DATE);"""
+sql_command = """CREATE TABLE IF NOT EXISTS vocaTable (
+voc_Num INTEGER PRIMARY KEY,
+unfamilarity INTEGER,
+word VARCHAR(30),
+synonym VARCHAR(40));"""
 
 # execute the statement
 crsr.execute(sql_command)
 
 # SQL command to insert the data in the table
-sql_command = """INSERT INTO emp VALUES (23, "Rishabh", "Bansal", "M", "2014-03-28");"""
+sql_command = """INSERT OR IGNORE INTO vocaTable VALUES (1, 0, "auspitious", "propitious");"""
 crsr.execute(sql_command)
 
 # another SQL command to insert the data in the table
-sql_command = """INSERT INTO emp VALUES (1, "Bill", "Gates", "M", "1980-10-28");"""
+sql_command = """INSERT OR IGNORE INTO vocaTable VALUES (2, 0, "a raft of", "a lot of");"""
 crsr.execute(sql_command)
 
-# execute the command to fetch all the data from the table emp
-crsr.execute("SELECT * FROM emp")
+# execute the command to fetch all the data from the table vocaTable
+crsr.execute("SELECT * FROM vocaTable")
 
 # store all the fetched data in the ans variable
 ans= crsr.fetchall()
