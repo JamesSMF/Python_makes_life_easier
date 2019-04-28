6c1d3b093ad4d3168f0dd1962cd56bb5e1f26ced
江南吴彦祖 <gli38@ucsc.edu>
2019-04-28 01:58:27 -0700

    auto-update by my python script

--- python/Gui/cld.py
+++ python/Gui/cld.py
@@ -21,23 +21,25 @@ def longestName(theDic):
     longest = max(theDic.values(), key=len)
     return len(longest)
 
+# This function takes an input of ordered dict() and sort it by key (FST in PAIR)
+def sortDict(assignment):
+    assignment = OrderedDict(sorted(assignment.items(), key=lambda x: int(x[0])))
+
 
 ################### GUI CODE #######################
 
 root = Tk()
-root.geometry("853x623")
+root.geometry("960x710")
 # root.resizeable(0,0)
 
 # analysis by James: This initializes the app frame size.
-bigFrame = Frame(root, width = 850, height = 620, relief = "raise")
+bigFrame = Frame(root, width = 960, height = 710, relief = "raise")
 bigFrame.grid()
 bigFrame.place
 
+############### BACKEND CODE #################
 
-
-############### BACKEND CODE ################
-
-# -------------- Start-up Preparition
+# -------------- Start-up Preparition -------------- #
 
 assignment = dict()   # restructured: map from due dates to assignment
 if os.stat("DataBase.db").st_size != 0:
@@ -72,11 +74,11 @@ if os.stat("Weekly.db").st_size != 0:
     # end for
 
     # sort the dictionary by date and time
-    assignment = OrderedDict(sorted(assignment.items(), key=lambda x: int(x[0])))
+    sortDict(assignment)
 
     # delete expired assignments
     # key is the date
-    loopIndice = 0.2
+    loopIndice = 0.07
     for key in list(assignment):
         stringDate = key[:8]      # take out YMD only
         copy = assignment[key]
@@ -86,54 +88,47 @@ if os.stat("Weekly.db").st_size != 0:
         #### Print out message of deletion of past events ###
             pastLabel = Label(bigFrame, text = (copy + " is automatically deleted because it is in the past now."), background="red", font = ("osaka", 18))
             pastLabel.grid()
-            loopIndice += 0.05
-            pastLabel.place(relx = 0.12, rely = loopIndice)
+            loopIndice += 0.04
+            pastLabel.place(relx = 0.17, rely = loopIndice)
 
         #####################################################
 
         # end if
     # end for
-# end if
+# end
+
+if os.stat("DataBase.db").st_size != 0:
+    sortDict(assignment)
+requestLabel = Label(bigFrame, text = "This box is for direct command operations", font = ("times", 18))
+requestLabel.grid()
+cmdBox = loopIndice + 0.08
+requestLabel.place(relx = 0.35, rely = cmdBox)
+
+cmdOpenField = Entry(bigFrame, width = 37)
+cmdOpenField.grid()
+cmdOpenField.place(relx = 0.33, rely = cmdBox+0.05)
+
 
 ################### BACKEND CODE ENDS HERE ####################
 
-
 indQuizOpenField = Entry(bigFrame, width = 15)
 indQuizOpenField.grid()
-indQuizOpenField.place(relx = 0.4, rely = 0.2)
+indQuizOpenField.place(relx = 0.43, rely = 0.9)
 
-Welcome = Label(bigFrame, text = "Welcome to Todo App Developed by James Li", font = ("avenir", 18))
+Welcome = Label(bigFrame, text = "Welcome to Todo App Developed by James Li", font = ("avenir", 16))
 Welcome.grid()
-Welcome.place(relx = 0.3, rely = 0.05)
+Welcome.place(relx = 0.33, rely = 0.01)
 
-todayLabel = Label(bigFrame, text=("Today is " + datetime.today().strftime('%Y-%m-%d') + "."), font=("avenir", 18))
+todayLabel = Label(bigFrame, text=("Today is " + datetime.today().strftime('%Y-%m-%d') + "."), font=("avenir", 16))
 todayLabel.grid()
-todayLabel.place(relx=0.4, rely=0.10)
+todayLabel.place(relx=0.43, rely=0.04)
 
-thanksLabel = Label(bigFrame, text="Thanks for using this app. Have a nice one :)", font=("avenir", 18))
+thanksLabel = Label(bigFrame, text="Thanks for using this app. Have a nice one :)", font=("avenir", 16))
 thanksLabel.grid()
-thanksLabel.place(relx=0.32, rely=0.15)
+thanksLabel.place(relx=0.35, rely=0.07)
 
 indQuizButton = Button(bigFrame, text = "Open Ind Quiz", style="TButton", command = longestName(assignment))
 indQuizButton.grid()
 indQuizButton.place(relx = 0.42, rely = 0.6)
 
-###############################################################################################################
-
-# Analysis by James: Entry is for user input, and it takes two paras: (frame, width)
-grpQuizOpenField = Entry(bigFrame, width = 15)
-grpQuizOpenField.grid()
-
-# Analysis by James: relx and rely are the coordinate of the entry box
-grpQuizOpenField.place(relx = 0.4, rely = 0.5)
-
-grpQuizLabel = Label(bigFrame, text = "Enter Grp Quiz Name:", font = ("helvetica", 10))
-grpQuizLabel.grid()
-grpQuizLabel.place(relx = 0.4, rely = 0.4)
-
-grpQuizButton = Button(bigFrame, text = "Open Grp Quiz", style="TButton", command = longestName(assignment))
-grpQuizButton.grid()
-grpQuizButton.place(relx = 0.42, rely = 0.6)
-
-###############################################################################################################
 root.mainloop()
