eec17853f8d41a65442d728d458ddc5e3ad1e04d
江南吴彦祖 <gli38@ucsc.edu>
2019-05-27 21:19:58 -0700

    auto-update by my python script

--- java/Encode.java
+++ java/Encode.java
@@ -1,147 +1,30 @@
 import java.io.*;
-import java.lang.Math;
 
-class Encode{
-   public static void main(String[] args) throws IOException{
-      System.out.print("Enter the number to be encoded: ");
-      System.out.flush();
-      double input = getDouble();    // get the input
-      int fstBit = (input > 0 ? 0 : 1);
-
-      // split input into decimal and integer parts
-      String inputString = String.valueOf(input);
-      int index = inputString.indexOf(".");
-      int intPart = Integer.parseInt(inputString.substring(0,index));
-      String rep = inputString.substring(index).replace(".","");
-      int decPart = Integer.parseInt(rep);
-
-      // calculates for binary for each part
-      int[] bInt = decToBinary(intPart, 4);
-      int[] bDec = decPartBin(decPart, 4);
-      int bias = bInt.length + 126;
-      int[] Bbias = decToBinary(bias, 8);
-
-      // combine and get a binary array
-      int[] combArray = combine(bInt, bDec);
-      printArray(bInt);
-      // printArray(bDec);
-      combArray = combine(Bbias, combArray);
-      // printArray(combArray);
-      int [] temp = {fstBit};
-      combArray = combine(temp, combArray);
-      // printArray(combArray);
-
-      // convert the combined binary array to an array of hex
-      char[] hex = new char[8];
-      int slot = 0;
-      char ans = '0';
-      for(int i=0; i<32; i++){
-         if((i+1)%4==0){
-            int newInt = combArray[i-3] * 8 + combArray[i-2] * 4 + combArray[i-1] * 2 + combArray[i];
-            // System.out.println(newInt);
-            if(newInt > 9){
-               switch(newInt){
-                  case 10:
-                     ans = 'A';
-                     break;
-
-                  case 11:
-                     ans = 'B';
-                     break;
-
-                  case 12:
-                     ans = 'C';
-                     break;
-
-                  case 13:
-                     ans = 'D';
-                     break;
-
-                  case 14:
-                     ans = 'E';
-                     break;
-                
-                  case 15:
-                     ans = 'F';
-                     break;
-               }   // end of switch
-               hex[slot++] = ans;
-            }else{
-               hex[slot++] = (char) (newInt + '0');
-               // System.out.println(ans);
-            }  // end of String conversion
-         }
-      }
-      printArray(hex);
+public class Encode{
+   // Get 32-bit IEEE 754 format of the decimal value  
+   private static String GetBinary32( float value ) throws IOException{  
+      int intBits = Float.floatToIntBits(value); 
+      String binary = Integer.toBinaryString(intBits);
+      return binary;
    }
 
-   static String getString() throws IOException{
+   private static String getString() throws IOException{
       InputStreamReader isr = new InputStreamReader(System.in);
       BufferedReader br = new BufferedReader(isr);
-      String s = br.readLine();
-      return s;
+      return br.readLine();
    }
-
-   static double getDouble() throws IOException{
-      String s = getString();
-      return Double.parseDouble(s);
+ 
+   /**
+   * @param args
+   */
+   public static void main(String[] args) throws IOException{
+      System.out.print("Please enter the number to Encode: ");
+      String input = getString();
+      float f = Float.valueOf(input.trim()).floatValue();
+      String str = GetBinary32(f);
+      int decimal = Integer.parseInt(str,2);
+      String hexStr = Integer.toString(decimal,16);
+      System.out.print( "Encoding result of " + input + ": ");  
+      System.out.println(hexStr);
    }
-
-
-   static int[] decToBinary(int n, int len){
-      int[] binaryNum = new int[len];
-   
-      while (n > 0){ 
-         // storing remainder in binary array 
-         if(len==0) break;
-         binaryNum[--len] = n % 2; 
-         n = n / 2;
-      }
-      
-      return binaryNum;  
-   }
-
-   static int[] decPartBin(int n, int len){
-      int[] binaryNum = new int[len];
-
-      int whatever = 10;
-      while()
-
-      double target = 0.5 * Math.pow(10,len);
-      // System.out.println(target);
-      int count = 0;
-      while(n>0){
-         while(n < target){
-            target /= 2;
-            binaryNum[count++] = 0;
-         }
-
-         n -= target;
-         binaryNum[count++] = 1;
-      }
-
-      return binaryNum;
-   }
-
-   static int[] combine(int[] a, int[] b){
-      int length = a.length + b.length;
-      int[] result = new int[length];
-      System.arraycopy(a, 0, result, 0, a.length);
-      System.arraycopy(b, 0, result, a.length, b.length);
-      return result;
-   }
-
-   static void printArray(char[] theArray){
-      for(int i=0; i<theArray.length; i++)
-         System.out.print(theArray[i]);
-      System.out.println();
-   }
-
-   static void printArray(int[] theArray){
-      for(int i=0; i<theArray.length; i++)
-         System.out.print(theArray[i]);
-      System.out.println();
-   }
-
 }
-
---
+++ java/ToBinary.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ToBinary{
+   public static void main(String[] args){
+      int n, a;
+      String x = "";
+      Scanner s = new Scanner(System.in);
+      System.out.print("Enter any decimal number:");
+      n = s.nextInt();
+
+      while(n > 0){
+         a = n % 2;
+         x = x + "" + a;
+         n = n / 2;
+      }
+      System.out.println("Binary number:"+x);
+   }
+}
