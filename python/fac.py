01015210b3ccc3f24793c586ca63bb08ac2a88c1
James Li <gli38@ucsc.edu>
2019-06-05 14:00:55 -0700

    auto-update by my python script

--- python/fac.py
+++ python/fac.py
@@ -1,15 +1,15 @@
 import sys
 
 def factorial(x):
-    if(not x.isdigit()):
-        return -1
-    return reduce(lambda x,y: x * y, [1]+range(1, int(x)+1))
-    # prepend a [1] to prevent the input being 0
+   if(not x.isdigit()):
+      return -1
+   return reduce(lambda x,y: x * y, [1]+range(1, int(x)+1))
+   # prepend a [1] to prevent the input being 0
 
 inp = raw_input("please enter the number: ")
 
 out = factorial(inp)
 if(out==-1):
-    print("Please enter a valid number.")
+   print("Please enter a valid number.")
 else:
-    print(out)
+   print(out)
