2e29886daac86ebf628a28e36fdb72816382847b
江南吴彦祖 <gli38@ucsc.edu>
2019-06-09 15:09:23 -0700

    auto-update by my python script

---
+++ java/swapInt.java
@@ -0,0 +1,11 @@
+public class Final{
+   public static void main(String[] args){
+      int x=10, y=20;
+      x = swap(y, y=x);
+      System.out.println(x+","+y);
+   }
+
+   static int swap(int egg, int potato){
+      return egg;
+   }
+}
