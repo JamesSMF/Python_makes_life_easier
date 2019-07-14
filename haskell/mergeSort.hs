e592b696261a9dbff97da8c73936886a9dd80535
江南吴彦祖 <gli38@ucsc.edu>
2019-07-14 16:43:43 +0800

    auto-update by my python script

---
+++ haskell/mergeSort.hs
@@ -0,0 +1,18 @@
+-- divide and conquer: merge sort
+sort :: (Ord a) => [a] -> [a]
+sort [] = []
+sort [x] = [x]
+sort xs = merge (sort ys) (sort zs)
+          where (ys, zs) = half xs
+
+-- divide
+half :: (Ord a) => [a] -> ([a], [a])
+half xs = (take n xs, drop n xs)
+          where n = length xs `div` 2
+
+-- conquer
+merge :: (Ord a) => [a] -> [a] -> [a]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys) = if   x <= y then x:merge xs (y:ys)
+                      else y:merge (x:xs) ys
