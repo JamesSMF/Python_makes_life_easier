5d90418bdc311494a19810228f7310c0ba3285ea
江南吴彦祖 <gli38@ucsc.edu>
2019-07-21 02:07:44 +0800

    auto-update by my python script

---
+++ java/specialSwap.java
@@ -0,0 +1,13 @@
+class specialSwap{
+   public static void main(String[] args){
+      int x = 3;
+      int y = 4;
+
+      // ^ means bitwise XOR
+      x = x^y;
+      y = x^y;      // y = (x^y)^y = x^0 = x
+      x = x^y;      // x = (x^y)^x = y^0 = y
+
+      System.out.println("x = " + x + " , y = " + y);
+   }
+}
