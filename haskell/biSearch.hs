383c982f73478e30d12a6daff261c1ff3c20b4b2
江南吴彦祖 <gli38@ucsc.edu>
2019-08-07 03:04:38 +0800

    auto-update by my python script

--- haskell/biSearch.hs
+++ haskell/biSearch.hs
@@ -1,8 +1,8 @@
 binsearch :: [Int] -> Int -> Int -> Int -> Int -- list, value, low, high, return int
 binsearch xs value low high
-   | high < low       = -1           -- Not found
+   | high < low       = -1                                  -- Not found
    | xs!!mid > value  = binsearch xs value low (mid-1)      -- search lower half
    | xs!!mid < value  = binsearch xs value (mid+1) high     -- search upper half
-   | otherwise        = mid          -- Found
+   | otherwise        = mid                                 -- Found
    where
    mid = low + ((high - low) `div` 2)
