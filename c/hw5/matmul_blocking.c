8707d8a7ae2d3a7a8159b66a058881a26a9d59c1
James Li <gli38@ucsc.edu>
2019-11-24 12:31:32 -0800

    auto-update by my python script

--- c/hw5/matmul_blocking.c
+++ c/hw5/matmul_blocking.c
@@ -84,6 +84,7 @@ int main(int argc, char **argv){
 
    init(A, B);
    memset((__uint64_t**)C, 0, sizeof(__uint64_t) * SIZE * SIZE);
+   memset((__uint64_t**)D, 0, sizeof(__uint64_t) * SIZE * SIZE);
    
    t = clock();
 
