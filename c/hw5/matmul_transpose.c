1474cc0c83bec7506a9e4624fd175d9fda63cbfd
江南吴彦祖 <gli38@ucsc.edu>
2019-11-24 16:07:07 -0800

    auto-update by my python script

--- c/hw5/matmul_transpose.c
+++ c/hw5/matmul_transpose.c
@@ -9,6 +9,7 @@
 volatile __uint64_t A[SIZE][SIZE];
 volatile __uint64_t B[SIZE][SIZE];
 volatile __uint64_t C[SIZE][SIZE];
+volatile __uint64_t D[SIZE][SIZE];
 
 void init(volatile __uint64_t A[][SIZE], volatile __uint64_t B[][SIZE]){
    int r, c;
@@ -63,7 +64,19 @@ void modified_matmul(volatile __uint64_t A[][SIZE], volatile __uint64_t B[][SIZE
    for (rowA = 0; rowA < SIZE; rowA++) {
       for (colB = 0; colB < SIZE; colB++) {
          for (idx = 0; idx < SIZE; idx++) {
-            C[rowA][colB] += A[rowA][idx] * B[rowA][idx];
+            D[rowA][colB] += A[rowA][idx] * B[rowA][idx];
+         }
+      }
+   }
+}
+
+void matmul(volatile __uint64_t A[][SIZE], volatile __uint64_t B[][SIZE]){
+   int rowA, colB, idx;
+
+   for (rowA = 0; rowA < SIZE; rowA++) {
+      for (colB = 0; colB < SIZE; colB++) {
+         for (idx = 0; idx < SIZE; idx++) {
+            C[rowA][colB] += A[rowA][idx] * B[idx][colB];
          }
       }
    }
@@ -75,12 +88,14 @@ int main(int argc, char **argv){
 
    init(A, B);
    memset((__uint64_t**)C, 0, sizeof(__uint64_t) * SIZE * SIZE);
+   memset((__uint64_t**)D, 0, sizeof(__uint64_t) * SIZE * SIZE);
    
    transpose(B);
    t = clock();
    modified_matmul(A, B);
    t = clock() - t;
    time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+   verify(C,D);
    
    printf("Matmul took %f seconds to execute \n", time_taken);
 }
