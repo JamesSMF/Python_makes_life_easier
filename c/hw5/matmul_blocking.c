c322138ebd7dbfe12678df01ae2ebdf7c1dc0e73
江南吴彦祖 <gli38@ucsc.edu>
2019-11-24 22:53:07 -0800

    auto-update by my python script

--- c/hw5/matmul_blocking.c
+++ c/hw5/matmul_blocking.c
@@ -9,7 +9,6 @@
 volatile __uint64_t A[SIZE][SIZE];
 volatile __uint64_t B[SIZE][SIZE];
 volatile __uint64_t C[SIZE][SIZE];
-volatile __uint64_t D[SIZE][SIZE];
 
 void init(volatile __uint64_t A[][SIZE], volatile __uint64_t B[][SIZE]){
    int r, c;
@@ -38,45 +37,23 @@ int verify(volatile __uint64_t C[][SIZE], volatile __uint64_t D[][SIZE]){
    return 0;
 }
 
-void matmul(volatile __uint64_t A[][SIZE], volatile __uint64_t B[][SIZE]){
-   int rowA, colB, idx;
-
-   for (rowA = 0; rowA < SIZE; ++rowA) {
-      for (colB = 0; colB < SIZE; ++colB) {
-         for (idx = 0; idx < SIZE; ++idx) {
-            C[rowA][colB] += A[rowA][idx] * B[idx][colB];
-         }
-      }
-   }
-}
-
-
-// TODO: the result is not correct.
-
 void blocking_mult(volatile __uint64_t A[][SIZE], volatile __uint64_t B[][SIZE]
-      ,volatile __uint64_t C[][SIZE], int block_size){
-   int gk=0, gj=0, k, j, i=0;
+      , int block_size){
+   int gi, gk, gj, k, j, i;
    __uint64_t sum=0;
    int number_block = SIZE / block_size;
-   /* int number_block = block_size * (SIZE / block_size); */
 
-   // gk, gj traverses through the matrix according to blocks
-   // k, j traverses each block and calculate those motherfucking shitty god damn values
-   // k stands for rows num in A and col num in B
-   for(;gk<number_block; gk+=block_size){
-      for(;gj<number_block; gj+=block_size){
-         for(;i<SIZE; ++i){
-            for(j=gj; j<gj+block_size; ++j){
-               sum = C[i][j];
-               for (k=gk; k<gk+block_size; ++k)
-                  sum += A[i][k] * B[k][j];
-               C[i][j] = sum;
-            }
-         }
-      }
-   }
+   // gi, gk, gj traverses through the matrix according to blocks
+   // i, k, j traverses each block and calculate those motherfucking shitty god damn values
+   for(gi=0; gi<SIZE; gi+=block_size)
+      for(gj=0; gj<SIZE; gj+=block_size)
+         for(gk=0; gk<SIZE; gk+=block_size)
+            for(i=0 ; i<block_size; ++i)
+               for(j=0 ; j<block_size; ++j)
+                  for(k=0; k<block_size; ++k)
+                     C[gi+i][gj+j] += A[gi+i][gk+k] * B[gk+k][gj+j];
 
-   // I'm fucked up after writing this seemingly O(n^5) shitty algorithm.
+   // I'm fucked up after writing this seemingly O(n^6) shitty algorithm.
 }
 
 int main(int argc, char **argv){
@@ -85,17 +62,13 @@ int main(int argc, char **argv){
 
    init(A, B);
    memset((__uint64_t**)C, 0, sizeof(__uint64_t) * SIZE * SIZE);
-   memset((__uint64_t**)D, 0, sizeof(__uint64_t) * SIZE * SIZE);
    
    t = clock();
 
    // change the size for block (namely the last arg) to test different cases.
-   blocking_mult(A, B, D, 1024);
+   blocking_mult(A, B, 1024);
    t = clock() - t;
    time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
-
-   matmul(A,B);
-   verify(C,D);
    
    printf("Matmul took %f seconds to execute \n", time_taken);
 }
