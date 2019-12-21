1d1c7dd3d04690ee980c59b31c6cf2f5e3510b45
江南吴彦祖 <gli38@ucsc.edu>
2019-12-21 19:39:16 +0800

    auto-update by my python script

---
+++ java/Spring/spring-boot-01-helloworld/src/main/java/com/example/demo/SpringBoot01HelloworldApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class SpringBoot01HelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot01HelloworldApplication.class, args);
+	}
+
+}
+
+@Controller 
+class HelloController{
+   // It receives the request "hello" sended from the browser
+   @RequestMapping("/")
+   @ResponseBody
+   public String hello() {
+      return "Hello Spring Boot";
+   }
+}
