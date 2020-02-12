633d77856324facd0e2ab2faadfbc6a4a6b0e6d3
James Li <gli38@ucsc.edu>
2020-02-12 14:24:57 -0800

    auto-update by my python script

---
+++ python/AI/Neu_import.py
@@ -0,0 +1,33 @@
+# TensorFlow
+%tensorflow_version 2.x
+import tensorflow as tf
+
+# Proprocessing tools
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+# Keras models and layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM, GRU
+from tensorflow.keras.layers import SimpleRNN
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Reshape
+
+# Scientific computing
+import numpy as np
+
+# Plotting
+import matplotlib.pyplot as plt
+
+def plot_graphs(history, string):
+    '''
+    Plot training acc/loss and validation acc/loss
+    '''
+    plt.plot(history.history[string])
+    plt.plot(history.history['val_'+string])
+    plt.xlabel("Epochs")
+    plt.ylabel(string)
+    plt.legend([string, 'val_'+string])
+    plt.show()
